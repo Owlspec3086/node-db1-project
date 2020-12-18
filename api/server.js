0642bd44ac4058855f2f397eeb70dc0520b67e12
Owlspec3086 <ethical.worker3@gmail.com>
2020-12-17 19:00:22 -0500

    done

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
-const express = require("express");
-
-const db = require("../data/dbConfig.js");
-
+const express = require('express');
+const knex = require('../data/dbConfig.js');
+const accountRouter = require('../accounts/account-router');
+ 
 const server = express();
-
+const port = process.env.PORT || 5000
+ 
 server.use(express.json());
-
+server.use('/api/accounts', accountRouter);
+ 
+// server.use((err, req, res, next) => {
+//     console.log(err)
+//     res.status(500).json({
+//         message: "Check again, Try Harder",
+//     })
+// })
+ 
 module.exports = server;
